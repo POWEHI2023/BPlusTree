3ab1d0015b8136526875040479420d94a7c2869c
POWEHI2023 <zxe6b885@163.com>
2023-11-01 19:52:57 +0800

    a

---
+++ source/BPlusTree.h
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <functional>
+#include <cassert>
+
+namespace BPLUSTREE {
+
+}
+
+typedef struct NodeHeade {
+
+} NodeHeader;
+
+template <typename K>
+class BPlusNode;
+
+
+template <typename K, typename V>
+class BPlusLeaf;
+
+
+template <typename K, typename V>
+class BPlusTree;
+
+
+
+
+
+
+template <typename K>
+class BPlusNode {
+
+};
+
+template <typename K, typename V>
+class BPlusLeaf {
+
+};
+
+template <typename K, typename V>
+class BPlusTree {
+
+};
